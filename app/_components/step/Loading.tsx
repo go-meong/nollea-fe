0c0b2c8167aebdffd96b940d21fee9f215367a12
Jay Lee <roof2586@naver.com>
2025-07-10 23:23:45 +0900

    Loading 컴포넌트 수정: 텍스트 컴포넌트 추가 및 레이아웃 조정

--- app/_components/step/Loading.tsx
+++ app/_components/step/Loading.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Text } from "@vapor-ui/core";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -18,12 +19,12 @@ export default function Loading() {
   }, []);
 
   return (
-    <div>
-      <div className="font-bold text-[28px] text-center text-white mb-20">
+    <div className="mt-40 flex flex-col flex-1 relative">
+      <Text typography="heading2" style={{ color: "var(--vapor-color-white)" }} className="text-center mb-12">
         제주에 어떤 밤이
         <br />
         기다리고 있을까요?
-      </div>
+      </Text>
 
       <Image src="/loading.png" width={282} height={243} alt="loading" />
     </div>
