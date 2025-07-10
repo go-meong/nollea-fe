1c1f9067f31d94fafd66c7b2f0377eb7995ef74d
Jay Lee <roof2586@naver.com>
2025-07-11 04:01:56 +0900

    feat: 야간 투어 리스트를 API로 동적으로 로드하도록 변경

--- app/spots/page.tsx
+++ app/spots/page.tsx
@@ -1,25 +1,29 @@
 "use client";
 
 import CommonCard from "../_components/common/CommonCard";
+import { useNightTours } from "../_hooks";
 import Header from "./_components/Header";
 
 export default function Page() {
+  const { data: nightTours } = useNightTours();
+
   return (
     <div className="flex-1 relative">
       <Header />
       <div className="flex-1 flex flex-col mt-14">
-        <CommonCard
-          id="1"
-          imgUrl=""
-          badges={[]}
-          title="제주 야간 관광 리스트"
-          location="제주시"
-          userLat={33.486833}
-          userLon={126.514822}
-          lat={33.486833}
-          lon={126.514822}
-          time={["18:00", "20:00"]}
-        />
+        {nightTours.data.map((tour) => (
+          <CommonCard
+            key={tour.title}
+            id={tour.id}
+            imgUrl={tour.imageUrl}
+            badges={tour.categoryList}
+            title={tour.title}
+            location={tour.fullAddress}
+            time={tour.serviceHours}
+            lat={tour.coordinates?.[0]}
+            lon={tour.coordinates?.[1]}
+          />
+        ))}
       </div>
     </div>
   );
