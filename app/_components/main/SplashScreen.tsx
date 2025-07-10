70d044fb7b2abcf1920ea0a571600fa63aaf9c4e
Jay Lee <roof2586@naver.com>
2025-07-10 20:15:53 +0900

    feat: 스플래시 스크린 레이아웃 수정 및 텍스트 스타일 변경

--- app/_components/main/SplashScreen.tsx
+++ app/_components/main/SplashScreen.tsx
@@ -8,23 +8,17 @@ export default function SplashScreen() {
   const router = useRouter();
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-25">
+    <div className="h-screen flex flex-col items-center">
       {/* 상단 텍스트 */}
-      <div>
-        <Text foreground="accent" typography="body2">
+      <div className="mt-[172px] flex-1">
+        <Text typography="body2" style={{ color: "var(--vapor-color-white)" }}>
           제주도에서 즐기는 새로운 야간 문화
         </Text>
         <Image className="mt-4" src="/landing-title.png" width={203} height={56} alt="landing-title" />
       </div>
 
-      {/* 이미지 */}
-      <div className="w-screen max-w-[600px]">
-        <Image className="relative" src="/star1.png" width={282} height={244} alt="star" />
-        <Image className="absolute" src="/char1.png" width={100} height={204} alt="char" />
-      </div>
-
       {/* 버튼 */}
-      <Button className="w-80 h-12 bg-[#FF6500]" onClick={() => router.push("/home")}>
+      <Button className="w-80 h-12 bg-[#FF6500] mb-[113px]" onClick={() => router.push("/home")}>
         <Text foreground="accent" typography="body1">
           같이 밤보고멍, 시작해볼까요?
         </Text>
