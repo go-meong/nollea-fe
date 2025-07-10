7fd2de91d2819a93ad537ac0a7bbfa6c0e687f77
nno3onn <nno3onn@naver.com>
2025-07-10 23:18:34 +0900

    feat: zustand store 설정

---
+++ store/useSelectStore.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type TJoin = "혼자" | "친구" | "연인" | "가족" | "동료" | "기타";
+type TVehicle = "도보" | "대중교통" | "자가용" | "렌트카";
+type TMood = "차분" | "로맨틱" | "신남" | "특별함" | "힐링" | "기쁨";
+type TActivity = "산책" | "공연" | "맛집" | "체험";
+
+interface SelectState {
+  join: TJoin;
+  vehicle: TVehicle;
+  mood: TMood;
+  activity: TActivity;
+}
+
+interface SelectAction {
+  setJoin: (join: TJoin) => void;
+  setVehicle: (vehicle: TVehicle) => void;
+  setMood: (mood: TMood) => void;
+  setActivity: (activity: TActivity) => void;
+}
+
+export const useSelectStore = create<SelectState & SelectAction>()(
+  devtools((set) => ({
+    join: null,
+    vehicle: null,
+    mood: null,
+    activity: null,
+
+    setJoin: (join) => set({ join }),
+    setVehicle: (vehicle) => set({ vehicle }),
+    setMood: (mood) => set({ mood }),
+    setActivity: (activity) => set({ activity }),
+  }))
+);
