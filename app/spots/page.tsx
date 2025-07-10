f68e4ea37a29577c4e46e1492ef7371b981780ab
nno3onn <nno3onn@naver.com>
2025-07-11 05:56:16 +0900

    style: add gap

--- app/spots/page.tsx
+++ app/spots/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <div className="flex-1 relative">
       <Header />
-      <div className="flex-1 flex flex-col mt-14">
+      <div className="flex-1 flex flex-col mt-14 gap-5">
         {nightTours.data.map((tour) => (
           <CommonCard
             key={tour.title}
